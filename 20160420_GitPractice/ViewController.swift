d00d6c829841b942f6243fd62e430bbbac517736
tlsmooth89 <tlsmooth89@gmail.com>
2016-04-20 16:30:31 +0900

    Initial Commit

---
+++ 20160420_GitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20160420_GitPractice
+//
+//  Created by tlsmooth89 on 4/20/16.
+//  Copyright © 2016 yusuke.iwasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
